e301e7a1fbbf74ab3a93900e348b39b982204189
Paul Masurel <paul.masurel@gmail.com>
2013-02-17 18:06:26 +0100

    got rid of numpy

--- code/pearls/pearls2.py
+++ code/pearls/pearls2.py
@@ -1,37 +1,30 @@
-import numpy as np
 from math import ceil, log
 from memoize import memoize
 
-
 # Let's call that the "pignolage" algorithm
+def heavier(pop):
+  (anything, light, heavy, real) = pop
+  return (0, 0, heavy + anything, real + light)
 
+def lighter(pop):
+  (anything, light, heavy, real) = pop
+  return (0, light + anything, 0, real + heavy)
 
-PEARL_STATES = (
-  anything,
-  lighter_if_fake,
-  heavier_if_fake,
-  real
-) = range(4)
+def even(pop):
+  return (0, 0, 0, sum(pop))
 
+def pop_cost(pop):
+  (anything, light, heavy, real) = pop
+  return anything * 2 + light+heavy
 
-NB_PEARL_STATES = len(PEARL_STATES)
+def diff(pop_a, pop_b):
+  return tuple(a-b for (a,b) in zip(pop_a,pop_b))
 
-EVEN = np.zeros((NB_PEARL_STATES, NB_PEARL_STATES), np.int)
-EVEN[:, real] = 1
+def add(*pops):
+  return tuple(sum(els) for els in zip(*pops))
 
-HEAVIER = np.zeros((NB_PEARL_STATES, NB_PEARL_STATES), np.int)
-HEAVIER[heavier_if_fake, anything] = 1
-HEAVIER[real, lighter_if_fake] = 1
-HEAVIER[heavier_if_fake, heavier_if_fake] = 1
-HEAVIER[real, real] = 1
-
-LIGHTER = np.zeros((NB_PEARL_STATES, NB_PEARL_STATES), np.int)
-LIGHTER[lighter_if_fake,anything] = 1
-LIGHTER[lighter_if_fake, lighter_if_fake] = 1
-LIGHTER[real, heavier_if_fake] = 1
-LIGHTER[real, real] = 1
-
-MEASURE_OUTCOMES = ( HEAVIER, LIGHTER, EVEN )
+def minus(pop):
+  return tuple(-el for el in pop)
 
 def min_with_limit(g, theoretical_limit):
   res = g.next()
@@ -53,19 +46,15 @@ def max_with_limit(g, upper_limit):
 
 def population_after_measures(population, measure):
   (left, right) = measure
-  outcomes = []
-  pop_no_weighted = population - left - right
+  pop_no_weighted = add(population, minus(left), minus(right))
   # if the balance says the two plates are even
-  outcomes.append( pop_no_weighted + np.dot(left, EVEN) + np.dot(right,EVEN) )
-  nb_pearls_outside = pop_no_weighted.sum()
-  pop_outside_updated = np.zeros(4, np.int)
-  pop_outside_updated[real] = nb_pearls_outside
+  yield add(pop_no_weighted, even(left), even(right))
+  O = sum(pop_no_weighted)
   # if the balance says the left plate is lighter
-  outcomes.append( pop_outside_updated + np.dot(LIGHTER, left) + np.dot(HEAVIER, right) )
+  yield add( lighter(left), heavier(right), (0,0,0,O) )
   # if the balance says the left plate is heavier
-  outcomes.append( pop_outside_updated + np.dot(HEAVIER, left) + np.dot(LIGHTER, right) )
-  return outcomes
-
+  yield add( heavier(left), lighter(right), (0,0,0,O) )
+  
 def fill_plate_list(pop, plate_size):
   if len(pop)==1:
     if pop[0] >= plate_size:
@@ -77,16 +66,16 @@ def fill_plate_list(pop, plate_size):
 
 def fill_plate(population, plate_size):
   for plate in fill_plate_list(list(population), plate_size):
-    yield np.array(plate, np.int)
+    yield tuple(plate)
 
 def measures(population):
-  N = population.sum()
+  N = sum(population)
   cost = cost_branch(population)
   possible_plate_sizes = range(1, N/2+1)
   possible_plate_sizes.sort(key=lambda k:abs( 2*(N-2*k)-cost/3) )
   for plate_size in possible_plate_sizes:
     for left in fill_plate(population, plate_size):
-      remaining = population - left
+      remaining = diff(population, left)
       for right in fill_plate(remaining, plate_size):
         yield (left, right)
 
@@ -98,7 +87,7 @@ def cost_estimate(branches):
 
 def is_normalized(measure):
   (left, right) = measure
-  return tuple(left) >= tuple(right)
+  return left >= right
 
 def browse_solutions(population, limit):
   yield limit
@@ -106,7 +95,7 @@ def browse_solutions(population, limit):
   pops_updated_per_measure = []
   for measure in measures(population):
     if is_normalized(measure):
-      pops_updated_per_measure.append(population_after_measures(population, measure))
+      pops_updated_per_measure.append(list(population_after_measures(population, measure)))
   pops_updated_per_measure.sort(key=cost_estimate)
   for pops_updated in pops_updated_per_measure:
     measure_score = (
@@ -121,14 +110,15 @@ def browse_solutions(population, limit):
 def pearl_key(pop, limit):
   return (tuple(pop), limit)
 
-
 def solved(population):
-  if population[anything] > 0:
+  if population[0] > 0:
     return False
-  return population[lighter_if_fake] + population[heavier_if_fake] <= 1
+  else:
+    return population[1] + population[2] <= 1
 
 @memoize(key=pearl_key)
 def solve(pop, limit):
+  print pop
   if solved(pop):
     return 0
   theoretical_limit = ceil(log(cost_branch(pop),3))
@@ -137,9 +127,8 @@ def solve(pop, limit):
   solutions = browse_solutions(pop, limit)
   return min_with_limit(solutions, theoretical_limit)
 
-
 def pearl_pignolage(n):
   if n<3:
     return None
-  possibilities = np.array([n, 0, 0, 0], np.int)
-  return solve(possibilities, n*n)
+  pop = (n, 0, 0, 0)
+  return solve(pop, n*n)
